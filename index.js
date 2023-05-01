60bb80065f7cd984f7480ca099995780aa564890
pablo-guelu <pablo-guelu@gmail.com>
2023-05-01 17:57:45 +0200

    adding basic buttons to ck5

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import MathType from '@wiris/mathtype-ckeditor5/src/plugin';
 
 ClassicEditor
     .create(document.querySelector('#editor'), {
-        plugins: [MathType],
+        plugins: [
+            Essentials, Paragraph, Bold, Italic, MathType
+        ], 
         toolbar: {
-            items: ['MathType', 'ChemType'],
-        },
-    })
+            items: [
+                'bold', 'italic', 'alignment', 'MathType', 'ChemType'
+            ]
+        }})
     .catch(error => {
         console.error(error);
     });
